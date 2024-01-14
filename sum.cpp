16a7fda940ded706842ad62042ca285d2c937366
Vedmandal <mandalved643@gmail.com>
2024-01-14 17:19:21 +0530

    sum of binary tree

---
+++ sum.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class node{
+    public:
+    int val;
+    node*left;
+    node*right;
+    node(int val){
+        this->val=val;
+    }
+};
+
+int idx=0;
+node*construct_tree(vector<int>arr){
+    if(arr.size()==0 || arr[idx]==-1){
+        idx++;
+        return NULL;
+    }
+    node*root=new node(arr[idx++]);
+    root->left=construct_tree(arr);
+    root->right=construct_tree(arr);
+    return root;
+
+
+}
+int sum(node*root){
+    if(root==NULL){
+        return 0;
+    }
+    int left=sum(root->left);
+    int right=sum(root->right);
+    return root->val+left+right;
+
+}
+int main(){
+      vector<int>arr={50,40,30,-1,-1,45,-1,-1,60,62,-1,-1,75,-1,-1};
+      node*root=construct_tree(arr);
+      cout<<sum(root);
+      
+
+}
