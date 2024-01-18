22003ffe6a69c446203bba066904d99a6bd8b4c9
Vedmandal <mandalved643@gmail.com>
2024-01-18 15:01:04 +0530

    remove leaves

---
+++ removeLeafNode.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class node{
+    public:
+    int val;
+    node*left;
+    node*right;
+    node(int val){
+        this->val=val;
+    }
+};
+
+int idx=0;
+node*construct_tree(vector<int>arr){
+    if(arr.size()==0 || arr[idx]==-1){
+        idx++;
+        return NULL;
+    }
+    node*root=new node(arr[idx++]);
+    root->left=construct_tree(arr);
+    root->right=construct_tree(arr);
+    return root;
+
+
+}
+ node*removeLeafNode(node*root){
+    if(root==NULL){
+        return NULL;
+    }
+    
+    node*l=removeLeafNode(root->left);
+    node*r=removeLeafNode(root->right);
+    if(root->left==NULL && root->right==NULL){
+        return NULL;
+    }
+    root->left=l;
+    root->right=r;
+    return root;
+ }
+ void display(node*root){
+    if(root==NULL){
+        return;
+    }
+    string str=root->left!=NULL?to_string(root->left->val):".";
+    str+= "<-"+to_string(root->val)+"->";
+    str+=root->right!=NULL?to_string(root->right->val):".";
+    cout<<str<<endl;
+    display(root->left);
+    display(root->right);
+}
+int main(){
+      vector<int>arr={50,40,30,-1,-1,45,-1,-1,60,62,-1,-1,75,-1,-1};
+      node*root=construct_tree(arr);
+      display(removeLeafNode(root));
+     
+
+}
