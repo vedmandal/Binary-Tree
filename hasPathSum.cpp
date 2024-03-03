78befe9de39dbb55fb77834eeec3f2fdbdb14013
Vedmandal <mandalved643@gmail.com>
2024-03-03 14:13:05 +0530

    has path sum

---
+++ hasPathSum.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class node{
+    public:
+    int val;
+    node*left;
+    node*right;
+    node(int val){
+        this->val=val;
+    }
+};
+
+int idx=0;
+node*construct_tree(vector<int>arr){
+    if(arr.size()==0 || arr[idx]==-1){
+        idx++;
+        return NULL;
+    }
+    node*root=new node(arr[idx++]);
+    root->left=construct_tree(arr);
+    root->right=construct_tree(arr);
+    return root;
+
+
+}
+void display(node*root){
+    if(root==NULL){
+        return;
+    }
+    string str=root->left!=NULL?to_string(root->left->val):".";
+    str+= "<-"+to_string(root->val)+"->";
+    str+=root->right!=NULL?to_string(root->right->val):".";
+    cout<<str<<endl;
+    display(root->left);
+    display(root->right);
+}
+
+bool hasPathSum(node* root, int targetsum)
+{
+    if (root == nullptr)
+      return false;
+    if (root->left == nullptr && root->right == nullptr && targetsum - root->val == 0)
+      return true;
+
+      return hasPathSum(root->left, targetsum - root->val) || hasPathSum(root->right, targetsum - root->val);
+  }
+
+int main(){
+      vector<int>arr={50,40,30,-1,-1,45,-1,-1,60,62,-1,-1,75,-1,-1};
+      node*root=construct_tree(arr);
+    cout<<hasPathSum(root,90);
+
+}
