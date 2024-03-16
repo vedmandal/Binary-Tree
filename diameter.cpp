271259f3fb05ea330df00cddc7ed82107269d199
Vedmandal <mandalved643@gmail.com>
2024-03-16 17:55:16 +0530

    diameter

---
+++ diameter.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class node{
+    public:
+    int val;
+    node*left;
+    node*right;
+    node(int val){
+        this->val=val;
+    }
+};
+
+int idx=0;
+node*construct_tree(vector<int>arr){
+    if(arr.size()==0 || arr[idx]==-1){
+        idx++;
+        return NULL;
+    }
+    node*root=new node(arr[idx++]);
+    root->left=construct_tree(arr);
+    root->right=construct_tree(arr);
+    return root;
+
+
+}
+void display(node*root){
+    if(root==NULL){
+        return;
+    }
+    string str=root->left!=NULL?to_string(root->left->val):".";
+    str+= "<-"+to_string(root->val)+"->";
+    str+=root->right!=NULL?to_string(root->right->val):".";
+    cout<<str<<endl;
+    display(root->left);
+    display(root->right);
+}
+
+int height(node* node, int& diameter) {
+
+        if (!node) {
+            return 0;
+        }
+
+        int lh = height(node->left, diameter);
+        int rh = height(node->right, diameter);
+
+        diameter = max(diameter, lh + rh);
+
+        return 1 + max(lh, rh);
+    }
+       
+    int diameterOfBinaryTree(node* root) {
+        int diameter = 0;
+        height(root, diameter);
+        return diameter;
+        
+    }
+int main(){
+      vector<int>arr={50,40,30,-1,-1,45,-1,-1,60,62,-1,-1,75,-1,-1};
+      node*root=construct_tree(arr);
+      display(root);
+
+}
