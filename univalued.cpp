5e100a9218b9fbdb13275c3dc2a93990f2a8b29c
Vedmandal <mandalved643@gmail.com>
2024-03-23 18:15:01 +0530

    is univalued binary tree

---
+++ univalued.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class node{
+    public:
+    int val;
+    node*left;
+    node*right;
+    node(int val){
+        this->val=val;
+    }
+};
+
+int idx=0;
+node*construct_tree(vector<int>arr){
+    if(arr.size()==0 || arr[idx]==-1){
+        idx++;
+        return NULL;
+    }
+    node*root=new node(arr[idx++]);
+    root->left=construct_tree(arr);
+    root->right=construct_tree(arr);
+    return root;
+
+
+}
+void display(node*root){
+    if(root==NULL){
+        return;
+    }
+    string str=root->left!=NULL?to_string(root->left->val):".";
+    str+= "<-"+to_string(root->val)+"->";
+    str+=root->right!=NULL?to_string(root->right->val):".";
+    cout<<str<<endl;
+    display(root->left);
+    display(root->right);
+}
+
+ bool isUnivalTree(node* root) {
+        if(root==NULL){
+            return true;
+        }
+
+        if( root->left!=NULL and root->left->val!=root->val){
+            return false;
+        } 
+        
+        if( root->right!=NULL and root->right->val!=root->val){
+            return false;
+        }
+
+       return isUnivalTree(root->left) and isUnivalTree(root->right);
+        
+    }
+int main(){
+      vector<int>arr={50,40,30,-1,-1,45,-1,-1,60,62,-1,-1,75,-1,-1};
+      node*root=construct_tree(arr);
+      display(root);
+
+}
