480ebb1d2051891922e831223e2a9f3cbe0fb858
Vedmandal <mandalved643@gmail.com>
2024-03-27 18:00:37 +0530

    binary tree tilt

---
+++ binaryTreeTilt.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class node{
+    public:
+    int val;
+    node*left;
+    node*right;
+    node(int val){
+        this->val=val;
+    }
+};
+
+int idx=0;
+node*construct_tree(vector<int>arr){
+    if(arr.size()==0 || arr[idx]==-1){
+        idx++;
+        return NULL;
+    }
+    node*root=new node(arr[idx++]);
+    root->left=construct_tree(arr);
+    root->right=construct_tree(arr);
+    return root;
+
+
+}
+
+
+int sum(node* root, int& ans) {
+    if (root == nullptr)
+      return 0;
+
+    const int l = sum(root->left, ans);
+    const int r = sum(root->right, ans);
+    ans += abs(l - r);
+    return root->val + l + r;
+  }
+    int findTilt(node* root) {
+        int ans = 0;
+    sum(root, ans);
+    return ans;
+        
+    }
+void display(node*root){
+    if(root==NULL){
+        return;
+    }
+    string str=root->left!=NULL?to_string(root->left->val):".";
+    str+= "<-"+to_string(root->val)+"->";
+    str+=root->right!=NULL?to_string(root->right->val):".";
+    cout<<str<<endl;
+    display(root->left);
+    display(root->right);
+}
+int main(){
+      vector<int>arr={50,40,30,-1,-1,45,-1,-1,60,62,-1,-1,75,-1,-1};
+      node*root=construct_tree(arr);
+      display(root);
+
+}
