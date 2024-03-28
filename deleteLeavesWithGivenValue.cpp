fca39c183ffd47c3ac4d6e66939e099a0ee873dd
Vedmandal <mandalved643@gmail.com>
2024-03-28 16:37:09 +0530

    delete leaves with a given value

---
+++ deleteLeavesWithGivenValue.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class node{
+    public:
+    int val;
+    node*left;
+    node*right;
+    node(int val){
+        this->val=val;
+    }
+};
+
+int idx=0;
+node*construct_tree(vector<int>arr){
+    if(arr.size()==0 || arr[idx]==-1){
+        idx++;
+        return NULL;
+    }
+    node*root=new node(arr[idx++]);
+    root->left=construct_tree(arr);
+    root->right=construct_tree(arr);
+    return root;
+
+
+}
+void display(node*root){
+    if(root==NULL){
+        return;
+    }
+    string str=root->left!=NULL?to_string(root->left->val):".";
+    str+= "<-"+to_string(root->val)+"->";
+    str+=root->right!=NULL?to_string(root->right->val):".";
+    cout<<str<<endl;
+    display(root->left);
+    display(root->right);
+}
+
+
+ node* removeLeafNodes(node* root, int target) {
+        if(root==NULL){
+            return NULL;
+        }
+
+        
+
+        root->left=removeLeafNodes(root->left,target);
+        root->right=removeLeafNodes(root->right,target);
+        if(root->left==NULL and root->right==NULL and root->val==target){
+            return NULL;
+        }
+        return root;
+
+        
+    }
+int main(){
+      vector<int>arr={50,40,30,-1,-1,45,-1,-1,60,62,-1,-1,75,-1,-1};
+      node*root=construct_tree(arr);
+      display(root);
+
+}
